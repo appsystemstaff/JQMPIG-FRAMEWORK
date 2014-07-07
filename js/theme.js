a999d40c147ac5bf80003e6037151eea09f0b96f
Lee Wai Kwok <appmoney712@gmail.com>
2014-07-08 02:11:37 +0800

    Initial issue.

---
+++ js/theme.js
@@ -0,0 +1,16 @@
+ 
+$(".page").attr("data-theme","y");$("#hrdiv").attr("data-theme","y");$("#hrs").html("Football calendar");$("#htitle").html("Five Energy Company");$("#htext").html("50% off for Brazil Bikini Lady");$("#ftitle").html("All right reserved");$("#ftext").html("Tel:<a href='tel:12241225tel'>12241225xmas</a> Email:<a href='mailto:example@gmail.coms'>example@gmail.com</a>");$("#m1").html("Football calendar");$("#m2").html("Form");$("#m3").html("Kiss Me");$("#m4").html("Playground");$("#m5").html("Video");
+$("#videohr").html("Video");$("#kisshr").html("Kiss Me");$("#playbhr").html("Playground");
+$("#jsqr").html("");$("#appr").html("I love Bikini Lady Simulation -Goal Goal Hit Bra in Youtube");
+$("#google").attr("href","https://play.google.com/store/search?q=&c=apps");
+$("#facebk").attr("href","https://www.facebook.com/appmoney712.net");
+if(!localStorage.getItem("infhref"))localStorage.setItem("infhref","http://www.appmoney712.net");if(!localStorage.getItem('weekdaysun') || localStorage.getItem('weekdaysun')!='Sun'){
+	var sun = localStorage.setItem('weekdaysun','Sun');
+	var mon = localStorage.setItem('weekdaymon','Mon');
+	var tue = localStorage.setItem('weekdaytue','Tue');
+	var wed = localStorage.setItem('weekdaywed','Wed');
+	var thu = localStorage.setItem('weekdaythu','Thu');
+	var fri = localStorage.setItem('weekdayfri','Fri');
+	var sat = localStorage.setItem('weekdaysat','Sat');};$("#pigappr").html("I love Bikini Lady Simulation -Goal Goal Hit Bra in Youtube");
+	
+	
